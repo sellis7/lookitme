80c817a2aa008123c7e7d66a3cf7634a82b9e0e5
sellis7 <sellis7@users.noreply.github.com>
2016-03-07 14:55:29 -0500

    Devise authentication functional for gram creation

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -10,20 +10,61 @@ RSpec.describe GramsController, type: :controller do
 	end
 
 	describe "grams#new action" do
+
+		it "should require user to be logged in" do
+			get :new
+			expect(response).to redirect_to new_user_session_path
+		end
+
 		it "should successfully show new form" do
+			user = User.create(
+				email: 'test.test@gmail.com',
+				password: 'testtest',
+				password_confirmation: 'testtest'
+			)
+			sign_in user
 			get :new
 			expect(response).to have_http_status(:success)
 		end
+
 	end
 
 	describe "grams#create action" do
+
+		it "should require user to be logged in" do
+			post :create, gram: { message: "Hello" }
+			expect(response).to redirect_to new_user_session_path
+		end
+
 		it "should successfully create new gram in db" do
-			post :create, gram: {message: 'Hello!'}
+			user = User.create(
+				email: 'test.test@gmail.com',
+				password: 'testtest',
+				password_confirmation: 'testtest'
+			)
+			sign_in user
+			post :create, gram: { message: 'Hello!' }
 			expect(response).to redirect_to root_path
 			gram = Gram.last
 			expect(gram.message).to eq("Hello!")
+			expect(gram.user).to eq(user)
 		end
+
+		it "should properly validate errors" do
+			user = User.create(
+				email: 'test.test@gmail.com',
+				password: 'testtest',
+				password_confirmation: 'testtest'
+			)
+			sign_in user
+			post :create, gram: { message: '' }
+			expect(response).to have_http_status(:unprocessable_entity)
+			expect(Gram.count).to eq 0
+
+		end
+
 	end
+
 
 
 
